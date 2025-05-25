b80a0973d25e0b13413541807240c81f65d58b6b
elonmaph <elonmaph@gmail.com>
2025-05-25 23:55:47 +0700

    Update CDNClient.swift
    Customizable

--- Sources/StreamChat/APIClient/CDNClient/CDNClient.swift
+++ Sources/StreamChat/APIClient/CDNClient/CDNClient.swift
@@ -59,6 +59,7 @@ public extension CDNClient {
     }
 }
 
+/// Customizable
 /// Default implementation of CDNClient that uses Stream CDN
 class StreamCDNClient: CDNClient {
     static var maxAttachmentSize: Int64 { 100 * 1024 * 1024 }
